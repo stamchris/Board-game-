99f9799a62cc505582b6b38ee2f39f94e93cd393
rajendiran <vinojan.rajendiran@etu.unistra.fr>
2021-02-07 14:57:31 +0100

    Correction d'un bug sur le melange des cartes des pioches

--- serveur/src/Deck.cr
+++ serveur/src/Deck.cr
@@ -4,11 +4,11 @@ class Deck
 end
 
 class DeckSurvie < Deck
-    property cards : Array(CarteSurvie) = CARTES_SURVIE*4
+    property cards : Array(CarteSurvie) = (CARTES_SURVIE*4).shuffle!
 end
 
 class DeckTrahison < Deck
-    property cards : Array(CarteTrahison) = CARTES_TRAHISON*3
+    property cards : Array(CarteTrahison) = (CARTES_TRAHISON*3).shuffle!
 end
 
 
