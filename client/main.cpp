8eec299c81cefe03b3b6e10ab79e22e00c472078
SCHMIDT LUCAS <lucas.schmidt@etu.unistra.fr>
2021-03-06 00:37:15 +0100

    Make the project compilable to web
    These changes will allow the most heavy images to be loaded outside of
    QRC, whether via filesystem or HTTP. A new QML element CerbTextField is
    added to show the virtual keyboard of mobile devices on click.

--- client/main.cpp
+++ client/main.cpp
@@ -1,22 +1,44 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include <libgen.h>
+
+#ifdef EMSCRIPTEN
+	#include <emscripten.h>
+	#include <emscripten/val.h>
+#endif
+
+#include "backend.h"
 
 int main(int argc, char *argv[])
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#endif
+	#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+		QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	#endif
 
-    QGuiApplication app(argc, argv);
+	QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(QStringLiteral("qrc:/Board.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
-
-    return app.exec();
+	QQmlApplicationEngine engine;
+	const QUrl url(QStringLiteral("qrc:/Application.qml"));
+	QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+			 &app, [url](QObject *obj, const QUrl &objUrl) {
+		if (!obj && url == objUrl)
+			QCoreApplication::exit(-1);
+	}, Qt::QueuedConnection);
+	QString root_url;
+	#ifdef EMSCRIPTEN
+		std::string location_href = emscripten::val::global("location")["href"].as<std::string>();
+		char* copy = new char[location_href.size()];
+		strncpy(copy, location_href.c_str(), location_href.size());
+		root_url = QString(dirname(copy))+'/';
+		delete[] copy;
+	#else
+		root_url = engine.baseUrl().toString();
+	#endif
+	engine.rootContext()->setContextProperty("ROOT_URL", root_url);
+	Backend backend;
+	engine.rootContext()->setContextProperty("Backend", &backend);
+	engine.load(url);
+	return app.exec();
 }
