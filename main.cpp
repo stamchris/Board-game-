38210ada14770461bd6274a5d127f557c5b4caf3
SENSENBRENNER AMAURY <amaury.sensenbrenner@etu.unistra.fr>
2021-03-15 18:34:38 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QGuiApplication app(argc, argv);
-    app.setWindowIcon(QIcon("cerbere-victoire.jpg"));
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("client/Application.qml")));
