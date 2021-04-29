427ef11f21b1237d835f7c8f703d91669821f2e6
SCHMIDT LUCAS <lucas.schmidt@etu.unistra.fr>
2021-04-29 12:41:50 +0200

    Make the database optional
    This will allow to simplify testing as not everybody can afford to start
    a database.

--- serveur/src/main.cr
+++ serveur/src/main.cr
@@ -8,10 +8,24 @@ require	"mysql"
 
 game = Cerbere::Game.new
 
-DB.open("mysql://root@localhost/cerbere-bdd")	do	|db|
-puts	typeof(db)#DB::Database
-puts	"connection	ok"
-db.close
+connection_string : String = ""
+if(ARGV.size() >= 1)
+	connection_string = ARGV[0]
+else
+	cerbere_bdd : String? = ENV["CERBERE_BDD"]?
+	if(!cerbere_bdd.nil?())
+		connection_string = cerbere_bdd.not_nil!()
+	end
+end
+
+if(connection_string != "")
+	DB.open(connection_string) do |db|
+		puts typeof(db) #DB::Database
+		puts "connection ok"
+		db.close
+	end
+else
+	puts "Avertissement: Pas d'arguments et variable d'environnement CERBERE_BDD vide. BDD désactivée."
 end
 
 ws "/" do |socket|
